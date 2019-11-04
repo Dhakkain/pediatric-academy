1df24a20e054e0956a23345892789fcef2509d07
Michał Stachowiak <m.stachowiak94@gmail.com>
2019-11-04 22:20:25 +0100

    Init

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent implements OnInit {
+
+  people = ["Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa",
+  "Katarzyna Słodowa"]
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { SheduleComponent } from './shedule/shedule.component';
+import { ContactComponent } from './contact/contact.component';
+import { LecturersComponent } from './lecturers/lecturers.component';
+import {MatCardModule} from '@angular/material/card';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    SheduleComponent,
+    ContactComponent,
+    LecturersComponent,
+    NavbarComponent,
+    SponsorsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
